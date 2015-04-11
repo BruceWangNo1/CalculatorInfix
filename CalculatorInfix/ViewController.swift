740c7005fa4bc33c64b31a08e36f1fadec100200
BruceWangNo1 <2819931971@qq.com>
2015-04-12 03:07:07 +0900

    Added commments about this program

--- CalculatorInfix/ViewController.swift
+++ CalculatorInfix/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Bruce Wang on 3/14/15.
 //  Copyright (c) 2015 Bruce Wang. All rights reserved.
 //
+//  This is inspired from Stanford University Open Course Developing iOS Apps with Swift and modified by Bruce Wang
 
 import UIKit
 
